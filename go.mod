fc3106da5e08a7a9b1db1f444f40bb54c2386fc2
stridedot <1195775472@qq.com>
2023-10-14 13:48:51 +0800

    使用标准库处理字符

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/stridedot/crawler
+
+go 1.20
+
+require (
+	golang.org/x/net v0.17.0
+	golang.org/x/text v0.13.0
+)
--- main.go
+++ main.go
@@ -1,29 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io"
+	"log"
 	"net/http"
 )
 
 func main() {
-	resp, err := http.Get("https://www.bing.com/?mkt=zh-CN")
+	url := "https://www.thepaper.cn/"
+	body, err := Fetcher(url)
 	if err != nil {
-		fmt.Printf("Fetch url error: %v\n", err)
+		fmt.Printf("Fetcher error: %v\n", err)
 		return
+	}
+
+	fmt.Println(string(body))
+}
+
+// Fetcher 爬取数据
+func Fetcher(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Fetch url error: %v\n", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code: %v\n", resp.StatusCode)
-		return
+		return nil, fmt.Errorf("Error status code: %v\n", resp.StatusCode)
 	}
 
-	body, err := io.ReadAll(resp.Body)
+	bodyReader := bufio.NewReader(resp.Body)
+
+	// 爬取数据的编码
+	e := DetermineEncoding(bodyReader)
+
+	// 转换编码
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+
+	return io.ReadAll(utf8Reader)
+}
+
+// DetermineEncoding 判断爬取数据的编码
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
 	if err != nil {
-		fmt.Printf("Read body error: %v\n", err)
-		return
+		log.Printf("Fetcher error: %v", err)
+		return unicode.UTF8
 	}
 
-	fmt.Printf("%s\n", body)
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
