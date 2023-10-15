ff11941229afb301bc7695d007f7fe91d27911d6
stridedot <1195775472@qq.com>
2023-10-15 13:15:08 +0800

    interface

---
+++ collect/collect.go
@@ -0,0 +1,54 @@
+package collect
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Fetcher interface {
+	Get(url string) ([]byte, error)
+}
+
+type BaseFetch struct{}
+
+// Get 爬取数据
+func (f *BaseFetch) Get(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Fetch url error: %v\n", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Error status code: %v\n", resp.StatusCode)
+	}
+
+	bodyReader := bufio.NewReader(resp.Body)
+
+	// 爬取数据的编码
+	e := DetermineEncoding(bodyReader)
+
+	// 转换编码
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+
+	return io.ReadAll(utf8Reader)
+}
+
+// DetermineEncoding 判断爬取数据的编码
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		log.Printf("Fetcher error: %v", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
--- main.go
+++ main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
-	"io"
-	"log"
-	"net/http"
+	"github.com/stridedot/crawler/collect"
 )
 
 func main() {
 	url := "https://news.sina.com.cn/"
-	body, err := Fetcher(url)
+	var f collect.Fetcher = &collect.BaseFetch{}
+
+	body, err := f.Get(url)
 	if err != nil {
 		fmt.Printf("Fetcher error: %v\n", err)
 		return
@@ -26,39 +21,4 @@ func main() {
 	doc.Find(`h1[data-client="headline"] a[target="_blank"]`).Each(func(i int, s *goquery.Selection) {
 		fmt.Printf("Fetch title: %s\n", s.Text())
 	})
-}
-
-// Fetcher 爬取数据
-func Fetcher(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, fmt.Errorf("Fetch url error: %v\n", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("Error status code: %v\n", resp.StatusCode)
-	}
-
-	bodyReader := bufio.NewReader(resp.Body)
-
-	// 爬取数据的编码
-	e := DetermineEncoding(bodyReader)
-
-	// 转换编码
-	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
-
-	return io.ReadAll(utf8Reader)
-}
-
-// DetermineEncoding 判断爬取数据的编码
-func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
-	bytes, err := r.Peek(1024)
-	if err != nil {
-		log.Printf("Fetcher error: %v", err)
-		return unicode.UTF8
-	}
-
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
 }
